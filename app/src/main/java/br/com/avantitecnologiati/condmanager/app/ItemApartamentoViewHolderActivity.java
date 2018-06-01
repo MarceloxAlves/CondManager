a1de14232d582434a8ffee573b928d7902afd6ef
Jesus <jesusadadfilho@gmail.com>
2018-06-01 02:49:05 -0300

    telas de listar e cadastrar apartamen

--- app/src/main/java/br/com/avantitecnologiati/condmanager/app/ItemApartamentoViewHolderActivity.java
+++ app/src/main/java/br/com/avantitecnologiati/condmanager/app/ItemApartamentoViewHolderActivity.java
@@ -2,6 +2,8 @@ package br.com.avantitecnologiati.condmanager.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import br.com.avantitecnologiati.condmanager.R;
+
 
 public class ItemApartamentoViewHolderActivity extends AppCompatActivity {
 
